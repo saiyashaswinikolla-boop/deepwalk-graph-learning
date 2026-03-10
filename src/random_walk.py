50558c866ec90368f9d22b97c1328d965252d0d5
saiyashaswinikolla-boop <saiyashaswinikolla@gmail.com>
2026-03-10 18:51:54 +0530

    Update random_walk.py

--- src/random_walk.py
+++ src/random_walk.py
@@ -1,1 +1,14 @@
+import random
 
+def random_walk(graph, start, walk_length):
+
+    walk = [start]
+    current = start
+
+    for _ in range(walk_length - 1):
+
+        neighbors = graph[current]
+        current = random.choice(neighbors)
+        walk.append(current)
+
+    return walk
