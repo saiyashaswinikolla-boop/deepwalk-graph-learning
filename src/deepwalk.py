c793c673b39e5fe8014aea1a7b55f795cdda8ab1
saiyashaswinikolla-boop <saiyashaswinikolla@gmail.com>
2026-03-10 18:51:19 +0530

    Update deepwalk.py

--- src/deepwalk.py
+++ src/deepwalk.py
@@ -1,1 +1,23 @@
+import pandas as pd
 
+def load_graph(path):
+
+    edges = pd.read_csv(path, sep=" ", header=None)
+    edges.columns = ["node1", "node2"]
+
+    graph = {}
+
+    for _, row in edges.iterrows():
+
+        u = row["node1"]
+        v = row["node2"]
+
+        if u not in graph:
+            graph[u] = []
+        if v not in graph:
+            graph[v] = []
+
+        graph[u].append(v)
+        graph[v].append(u)
+
+    return graph
