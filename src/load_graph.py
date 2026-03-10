d167f9b6a9183b99ae50ddfc4393d25cb477e269
saiyashaswinikolla-boop <saiyashaswinikolla@gmail.com>
2026-03-10 18:52:38 +0530

    Update load_graph.py

--- src/load_graph.py
+++ src/load_graph.py
@@ -1,1 +1,8 @@
+from load_graph import load_graph
+from random_walk import random_walk
 
+graph = load_graph("../data/facebook_combined.txt")
+
+for node in list(graph.keys())[:5]:
+    walk = random_walk(graph, node, 10)
+    print(walk)
